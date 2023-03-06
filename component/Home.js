ee181062526b3659ede817f8aaef32d8138033da
zubairkhan1234 <zubairsaleem1210@gmail.com>
2023-03-06 18:50:10 +0500

    some changes

--- component/Home.js
+++ component/Home.js
@@ -1,21 +1,7 @@
 import React from 'react'
-import gsap from 'gsap';
 // import { ScrollTrigger }  from "gsap/ScrollTrigger";
-import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import styles from '../styles/Home.module.css'
 function HomeComp() {
-    gsap.registerPlugin(ScrollTrigger);
-
-    // target the element with a class of "green" - rotate and move TO 100px to the left over the course of 1 second. 
-    if (typeof document !== "undefined") {
-        gsap.to(document.querySelectorAll(".green"), { rotation: 360, x: 100, duration: 1 });
-        // your code with access to window or document object here 
-        gsap.from(document.querySelectorAll(".purple"), { rotation: -360, x: -100, duration: 1 });
-        gsap.fromTo(document.querySelectorAll(".blue"), { x: -100 }, { rotation: 360, x: 100, duration: 1 });
-    }
-    // target the element with a class of "purple" - rotate and move FROM 100px to the left over the course of 1 second. 
-
-    // target the element with a class of "blue" - rotate and move FROM 100px to the left, TO 100px to the right over the course of 1 second. 
 
     return (
         <div className={styles.projectContainer}>
@@ -25,9 +11,6 @@ function HomeComp() {
                 <p className={styles.discription}>
                     I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building <span className={styles.primary_color}>Mobile Application</span>.
                 </p>
-                <div className="box green"></div>
-                <div className="box purple"></div>
-                <div className="box blue"></div>
                 <div className={styles.lg_btn}>
                     <button>Check Out my Github Account</button>
                 </div>
