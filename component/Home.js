7b9cc19eb38beaf46e77bdd82d824dbe3a177628
zubairkhan1234 <zubairsaleem1210@gmail.com>
2023-03-09 15:05:36 +0500

    added Typwriter package and integrated

--- component/Home.js
+++ component/Home.js
@@ -1,12 +1,35 @@
 import React from 'react'
 // import { ScrollTrigger }  from "gsap/ScrollTrigger";
 import styles from '../styles/Home.module.css'
-function HomeComp() {
+import Typewriter from 'typewriter-effect';
 
+
+function HomeComp() {
     return (
         <div className='container'>
             <p>WELCOME TO MY PORTFOLIO</p>
             <h1>hello</h1>
+            {/* <Typewriter
+                onInit={(typewriter) => {
+                    typewriter.typeString('Hello World!')
+                        .callFunction(() => {
+                            console.log('String typed out!');
+                        })
+                        .pauseFor(2500)
+                        .deleteAll()
+                        .callFunction(() => {
+                            console.log('All strings were deleted');
+                        })
+                        .start();
+                }}
+            /> */}
+            <Typewriter
+                options={{
+                    strings: ['Hello', 'World'],
+                    autoStart: true,
+                    loop: true,
+                }}
+            />
         </div>
     )
 }
